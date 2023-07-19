79b0521b8ed799ddce613dab1fb1f1e17795e2ef
Max Rantil <rantil@pm.me>
2023-07-19 23:54:05 +0300

    improvements on the css for better screen resizing

--- script.js
+++ script.js
@@ -1,38 +1,70 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+document.addEventListener("DOMContentLoaded", function () {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+
+  function adjustScrollPosition() {
+    window.scrollBy(0, -40); // Adjust this value to match your header's height
+  }
+
+  window.addEventListener("hashchange", adjustScrollPosition);
+  window.addEventListener("load", adjustScrollPosition);
+
+  document.querySelectorAll('a[href^="#"]').forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      var id = this.getAttribute('href').substring(1); // Get the ID without '#'
+      var targetElement = document.getElementById(id);
+      var topOffset;
+
+      switch (id) {
+        case "home":
+        case "work-experience":
+          topOffset = 40;
+          break;
+        case "education":
+          topOffset = 60;
+          break;
+        case "projects":
+          topOffset = 110;
+          break;
+        default:
+          topOffset = 40;
+      }
+
+      var targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - topOffset;
+
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth"
+      });
+    });
   });
-}
 
-function adjustScrollPosition() {
-  window.scrollBy(0, -40); // Adjust this value to match your header's height
-}
+  window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
 
-window.addEventListener("hashchange", adjustScrollPosition);
-window.addEventListener("load", adjustScrollPosition);
-
-document.querySelectorAll('a[href^="#"]').forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    window.location.hash = ''; // Remove the current hash
-    window.location.hash = this.getAttribute('href');
+  document.querySelectorAll('a.link-external').forEach(link => {
+    link.target = '_blank';
+    link.rel = 'noopener noreferrer';
   });
-});
 
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-}
+  const navIcon = document.querySelector('.nav-icon');
+  const nav = document.querySelector('.nav-right');
 
-document.querySelectorAll('a.link-external').forEach(link => {
-  link.target = '_blank';
-  link.rel = 'noopener noreferrer';
+  navIcon.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
 });
